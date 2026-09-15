258b9b85c63857ebc34ca29808898f5f3010042a
Gia Huy Duong <n24dtcn038@student.ptithcm.edu.vn>
2026-09-15 12:01:05 +0700

    fix categorydto

--- medicine-service/src/main/java/org/example/medicineservice/client/CategoryClient.java
+++ medicine-service/src/main/java/org/example/medicineservice/client/CategoryClient.java
@@ -1,6 +1,6 @@
 package org.example.medicineservice.client;
 
-import org.example.categoryservice.entity.Category;
+import org.example.medicineservice.dto.CategoryDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,5 +9,5 @@ import org.springframework.web.bind.annotation.GetMapping;
 @FeignClient(name="category-service")
 public interface CategoryClient {
     @GetMapping("/api/categories/{id}")
-    ResponseEntity<Category> getCategoryById(@PathVariable("id") Long id);
+    ResponseEntity<CategoryDto> getCategoryById(@PathVariable("id") Long id);
 }
